d785f5602f1ed14791803878845b095b26a9eb73
xseeddev <xseeddev@outlook.com>
2024-08-25 09:26:01 +0000

    Login file refactored... Keeping oriignal code for now

--- login.py
+++ login.py
@@ -25,3 +25,24 @@ def login(logincount, client, user_id, password, api_key, secret_key):
             traceback.print_exc()
             sleep(5)
             return login(logincount)
+
+# Refactor
+def angel_login(user_data):
+    
+    user_id = user_data["user_id"]
+    password = user_data["user_id"]
+    api_key = user_data["api_key"]
+    secret_key = user_data["secret_key"]
+    username = user_data["name"]
+
+    try:
+        angel_obj = SmartConnect(api_key)
+        angel_session = angel_obj.generateSession(user_id,password,pyotp.TOTP(secret_key).now())
+        refreshToken = angel_session['data']['refreshToken']
+        if angel_session['status'] == True:
+            Utils.write_log(username + ": Login Success")
+            Utils.write_log(angel_obj.getProfile(refreshToken))
+            return "Status:Login Success",angel_obj
+    except:
+        Utils.write_log(username + ": Login Failed")
+        return "Status:Login Fail",None
